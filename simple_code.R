eccf0ee93c8042e3938db61238f25b5116926ea9
fortune2rw <fortunehormos215@gmail.com>
2026-01-24 22:03:16 +0000

    Update simple_code.R

--- simple_code.R
+++ simple_code.R
@@ -1,11 +1,14 @@
-#LITTLE INFORMATION ABOUT ME#
+#Define Variables
 name <- "Fortunate Omorotionmwan"
 school <- "Ulster University"
 gene <- "KRAS"
 location <- "Homo Sapien"
 
-paste0("Hi, my name is ", name,  
+#Create and print sentence
+sentence <- paste0("Hi, my name is ", name,  
              ", a student of ", school, 
              ". My favourite gene is ", gene,  
              " found in ",  location,"."
 )
+
+print(sentence)
